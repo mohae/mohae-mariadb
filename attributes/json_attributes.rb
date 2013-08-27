1ceda393ff0eb29a67e20f94644c95f1fdb773f8
Joel Scoble <joel@moehae.com>
2013-08-26 22:14:24 -0500

    fix json && bump

--- attributes/json_attributes.rb
+++ attributes/json_attributes.rb
@@ -1,4 +1,3 @@
-mariadb.server_root_password   = 'uPdOwntUrnaRound-1-9-8-9'
-mariadb.server_repl_password   = 'uPdOwntUrnaRound-1-9-8-9'
-mariadb.server_debian_bassword = 'uPdOwntUrnaRound-1-9-8-9'
-include_recipe "mariadb::server"
+node.default['mariadb']['server_root_password'] = 'uPdOwntUrnaRound-1-9-8-9'
+node.default['mariadb']['server_repl_password'] = 'uPdOwntUrnaRound-9-8-9'
+node.default['mariadb']['server_debian_password'] = 'uPdOwntUrnaRound-18-9'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,4 @@
 #
 include_recipe "mariadb"
 include_recipe "mariadb::client"
-node.default.mariadb.server_root_password = "uPdOwntUrnaRoundbIzarre89"
-node.default.mariadb.server_repl_password = "uPdOwntUrnaRoundbIzarre89"
-node.default.mariadb.server_debian_password = "uPdOwntUrnaRoundbIzarre89"
 include_recipe "mariadb::server"
